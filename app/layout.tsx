27f826d65b5f547bc23c194cd4d90d3a248c8081
Hirusha Danthasinghe <hirushadanthasinghe@gmail.com>
2026-09-01 13:43:35 +0530

    enabled vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/next";
+
 const geist = Geist({
   variable: "--font-geist",
   subsets: ["latin"],
@@ -26,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geist.variable} ${geistMono.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
